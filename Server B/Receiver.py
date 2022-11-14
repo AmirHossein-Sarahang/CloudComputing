2be63d985a290edf213e3fe2b4f7ab65499e1899
AmirHossein-Sarahang <amirsarahang@aut.ac.ir>
2022-11-14 14:36:02 +0330

    update b!

--- Server B/Receiver.py
+++ Server B/Receiver.py
@@ -3,24 +3,18 @@ import pika
 import sys
 
 AMQP_URL = "amqps://dfphuyyq:4FzUTXTxXZ5w3A20MRD3c6IIqTrOz4ZT@hawk.rmq.cloudamqp.com/dfphuyyq"
-temp = ""
 
 def main():
     connection = pika.BlockingConnection(pika.URLParameters(AMQP_URL))
     channel = connection.channel()
     channel.queue_declare(queue='hello')
-
-
-
     def callback(ch, method, properties, body):
         print(body.decode())
-
     channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
-
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
 
-
+def Recive():
     try:
         main()
     except KeyboardInterrupt:
--- Server B/main.py
+++ Server B/main.py
@@ -1,1 +1,3 @@
 import Receiver
+
+Receiver.Recive()
