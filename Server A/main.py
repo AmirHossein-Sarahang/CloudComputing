e2dc49c98d61649eeb3e7cac5c3f53f04f093150
AmirHossein-Sarahang <amirsarahang@aut.ac.ir>
2022-11-24 01:56:16 +0330

    update main!

--- Server A/main.py
+++ Server A/main.py
@@ -2,27 +2,31 @@ import DB
 import Sender
 import requests
 import shutil
+import S3DB
 from fastapi import FastAPI, File, UploadFile, Form
 
 app = FastAPI()
 
 @app.post("/POST/")
-async def handle_post(file: UploadFile = File(), Email: str = Form(), Description: str = Form(),URL: str = Form()):
+async def handle_post(file: UploadFile = File(), Email: str = Form(), Description: str = Form(), URL: str = Form()):
     try:
         id = DB.Insert(Description, Email, URL)
+        s1 = "Your ad was registered with ID : "
         if Sender.SendMessage(str(id)):
-            s = "Your ad was registered with ID : "
-            s += str(id)
-            return s
-        s = "./data/"
-        s += file.filename
+            s1 += str(id)
 
-        with open(s, "wb") as buffer:
+        s2 = "./data/"
+        s2 += str(id)
+        s2 += ".jpg"
+        with open(s2, "wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
 
+        S3DB.Upload_to_s3(s2, str(id))
+        return s1
     except():
         print()
 
 @app.get("/GET/")
 async def handle_post(id: str = Form()):
     return DB.ShowState(id)
+
