a578b5d48147ac7880e68608d819584f1840558b
AmirHossein-Sarahang <amirsarahang@aut.ac.ir>
2022-11-27 18:04:29 +0330

    hidden api

--- Server B/EmialSender.py
+++ Server B/EmialSender.py
@@ -1,12 +1,13 @@
 import requests
 
-
+# b67fb1a41b7ef718e1b6a487bd46a295-2de3d545-cf7f8e98
+# mailgun@sandboxc93629c668b84ca48c96e3c0431e0faa.mailgun.org
 def send_email(e, id, t):
     try:
         return requests.post(
             "https://api.mailgun.net/v3/sandboxc93629c668b84ca48c96e3c0431e0faa.mailgun.org/messages",
-            auth=("api", "b67fb1a41b7ef718e1b6a487bd46a295-2de3d545-cf7f8e98"),
-            data={"from": "mailgun@sandboxc93629c668b84ca48c96e3c0431e0faa.mailgun.org",
+            auth=("api", ""),
+            data={"from": "",
                   "to": [e],
                   "subject": "Advertisement registration",
                   "text": message(t, id)})
