62128a5d6377830a54dd1df761d058c7e76b9551
AmirHossein-Sarahang <amirsarahang@aut.ac.ir>
2022-11-27 18:05:02 +0330

    update main

--- Server B/main.py
+++ Server B/main.py
@@ -1,3 +1,45 @@
-import Receiver
 import EmialSender
+import ImageProcess
 import DB
+import os
+import pika
+import sys
+
+import S3DB
+#amqps://dfphuyyq:4FzUTXTxXZ5w3A20MRD3c6IIqTrOz4ZT@hawk.rmq.cloudamqp.com/dfphuyyq
+
+AMQP_URL = "amqps://gyjulrsw:MzcT8naV6UtpQX05P_1HMBTKDQNX_2ig@beaver.rmq.cloudamqp.com/gyjulrsw"
+def MainHandler(i):
+    test = DB.GetUrl(i)
+    result_image = ImageProcess.main(test[0])
+    #result_image = ImageProcess.main(S3DB.GetURL(i))
+    if  result_image == False:
+        DB.setstate(i, False)
+        EmialSender.send_email(test[1], i, False)
+    else:
+        DB.setstate(i, True)
+        DB.SetCategory(i, result_image)
+        EmialSender.send_email(test[1], i, True)
+
+
+
+def main():
+    connection = pika.BlockingConnection(pika.URLParameters(AMQP_URL))
+    channel = connection.channel()
+    channel.queue_declare(queue='hello')
+    def callback(ch, method, properties, body):
+        print("Message received : ", body.decode())
+        MainHandler(body.decode())
+    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+try:
+    main()
+except KeyboardInterrupt:
+    print('Interrupted')
+    try:
+        sys.exit(0)
+    except SystemExit:
+        os._exit(0)
+
