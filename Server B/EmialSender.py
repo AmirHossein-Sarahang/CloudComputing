9a04146e0cd9e351773cb15d90bf0eb56d1e38fe
AmirHossein-Sarahang <amirsarahang@aut.ac.ir>
2022-11-27 18:01:26 +0330

    update Email sender & reciver

--- Server B/EmialSender.py
+++ Server B/EmialSender.py
@@ -1,13 +1,24 @@
 import requests
 
 
-def send_email(e, id):
-    temp = "Congratulations!, Your ad was registered with unique number : "
-    temp += id
-    return requests.post(
-        "https://api.mailgun.net/v3/sandboxc93629c668b84ca48c96e3c0431e0faa.mailgun.org/messages",
-        auth=("api", "b67fb1a41b7ef718e1b6a487bd46a295-2de3d545-cf7f8e98"),
-        data={"from": "mailgun@sandboxc93629c668b84ca48c96e3c0431e0faa.mailgun.org",
-              "to": [e],
-              "subject": "Advertisement registration",
-              "text": temp})
+def send_email(e, id, t):
+    try:
+        return requests.post(
+            "https://api.mailgun.net/v3/sandboxc93629c668b84ca48c96e3c0431e0faa.mailgun.org/messages",
+            auth=("api", "b67fb1a41b7ef718e1b6a487bd46a295-2de3d545-cf7f8e98"),
+            data={"from": "mailgun@sandboxc93629c668b84ca48c96e3c0431e0faa.mailgun.org",
+                  "to": [e],
+                  "subject": "Advertisement registration",
+                  "text": message(t, id)})
+    except():
+        print("Error in Send Emial!")
+
+def message(t,i):
+    temp = ""
+    if t:
+        temp = "Congratulations!, Your ad was registered with unique number : "
+        temp += i
+        return temp
+    else:
+        temp = "Your ad is rejected!"
+        return temp
--- Server B/Receiver.py
+++ Server B/Receiver.py
@@ -3,13 +3,12 @@ import pika
 import sys
 
 AMQP_URL = "amqps://dfphuyyq:4FzUTXTxXZ5w3A20MRD3c6IIqTrOz4ZT@hawk.rmq.cloudamqp.com/dfphuyyq"
-
 def main():
     connection = pika.BlockingConnection(pika.URLParameters(AMQP_URL))
     channel = connection.channel()
     channel.queue_declare(queue='hello')
     def callback(ch, method, properties, body):
-        print(body.decode())
+        print()
     channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
@@ -18,7 +17,6 @@ def Recive():
     try:
         main()
     except KeyboardInterrupt:
-
         print('Interrupted')
         try:
             sys.exit(0)
