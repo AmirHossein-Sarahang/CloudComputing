2c33c8b677fcfe52acf8f49c5e54c087d2d742fd
AmrHossein Sarahang <amirsarahang@aut.ac.ir>
2022-12-08 03:34:03 -0800

    Update EmialSender.py

--- Server B/EmialSender.py
+++ Server B/EmialSender.py
@@ -1,7 +1,6 @@
 import requests
 
-# b67fb1a41b7ef718e1b6a487bd46a295-2de3d545-cf7f8e98
-# mailgun@sandboxc93629c668b84ca48c96e3c0431e0faa.mailgun.org
+
 def send_email(e, id, t):
     try:
         return requests.post(
